b44fabdf2428d98cfb489d497e300c90b8ee6bcc
Ionut Tamas <tamash_ionut@hotmail.com>
2017-08-07 00:14:44 +0300

    Wip on atomic extensions

--- Prometheus/Prometheus.Extensions/ModelExtensions.cs
+++ Prometheus/Prometheus.Extensions/ModelExtensions.cs
@@ -50,5 +50,29 @@
             // Return value does not matter - is only used for method marking
             return false;
         }
+
+        /// <summary>
+        /// Marks a method or event a class being called exactly once, in the presence of other conditions.
+        /// This can work also for private method or events names.
+        /// E.g. for class "Account", the expression "x => x.IsActive && x.IsCalledOnce("Initialized")"
+        /// specifies that the "Initialized" method should be called exactly once when the flag "IsActive" is true.
+        /// </summary>
+        public static bool IsCalledOnce(this object value, string method)
+        {
+            // Return value does not matter - is only used for method marking
+            return false;
+        }
+
+        /// <summary>
+        /// Marks a function a class being called exactly once, in the presence of other conditions.
+        /// This works for public class method names.
+        /// E.g. for class "Account", the expression "x => x.IsActive && x.Initialized().IsCalledOnce()"
+        /// specifies that the "Initialized" method is called only once when the flag "IsActive" is true.
+        /// </summary>
+        public static bool IsCalledOnce<T>(this T value)
+        {
+            // Return value does not matter - is only used for method marking
+            return false;
+        }
     }
 }
